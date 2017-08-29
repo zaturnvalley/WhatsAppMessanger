d389fa30529732212e4cede4479df648688a127b
Zach Mason <zacharycmason@gmail.com>
2017-08-29 10:32:59 -0700

    row conditionals for new accnt

--- WhatsAppMessenger/frmRegister.cs
+++ WhatsAppMessenger/frmRegister.cs
@@ -40,7 +40,13 @@ namespace WhatsAppMessenger
 			}
 			if(WhatsAppApi.Register.WhatsRegisterV2.RequestCode(txtPhoneNumber.Text,out password, "sms"))
 			{
-
+				if (!string.IsNullOrEmpty(password))
+					Save();
+				else
+				{
+					grbRequestCode.Enabled = false;
+					grbConfirmCode.Enabled = true;
+				}
 			}
 		}
 
@@ -52,7 +58,17 @@ namespace WhatsAppMessenger
 			Properties.Settings.Default.Password = password;
 			Properties.Settings.Default.FullName = txtFullName.Text;
 			Properties.Settings.Default.Save();
-
+			if(Globals.DB.Accounts.FindByAccountId(txtPhoneNumber.Text) == null)
+			{
+				AppData.AccountsRow row = Globals.DB.Accounts.NewAccountsRow();
+				row.AccountId = txtPhoneNumber.Text;
+				row.FullName = txtFullName.Text;
+				row.Password = password;
+				Globals.DB.Accounts.AddAccountsRow(row);
+				Globals.DB.Accounts.AcceptChanges();
+				Globals.DB.Accounts.WriteXml(string.Format("{0}\\accounts.dat", Application.StartupPath));
+			}
+			DialogResult = System.Windows.Forms.DialogResult.OK;
 		}
 		private void btnConfirm_Click(object sender, EventArgs e)
 		{
